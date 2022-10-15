7ddf68cef36c45bdf408e256913a81f92af333f9
dot-eagle <lionvic3@gmail.com>
2022-10-15 00:08:00 -0700

    12. Software is eating the World: Write a program that prints all possible combinations of two two-digit numbers.

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - prints all possible combinations of two two-digit numbers
+ *
+ * Return: 0 as success
+ */
+
+int main(void)
+{
+	int p, q;
+
+	for (p = 0; p < 100; p++)
+	{
+		for (q = 0; q < 100; q++)
+		{
+			if (p < q)
+			{
+				putchar((p / 10) + 48);
+				putchar((p % 10) + 48);
+				putchar(' ');
+				putchar((q / 10) + 48);
+				putchar((q % 10) + 48);
+				if (p != 98 || q != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
