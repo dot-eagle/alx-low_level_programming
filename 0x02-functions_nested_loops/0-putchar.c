bec1108dfc296087536b855913270200b8cb7332
dot-eagle <lionvic3@gmail.com>
2022-10-18 21:11:11 -0700

    commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include "main.h"
 /**
  * main - print out the characters
  *
@@ -7,7 +7,6 @@
  *  Return: 0 return integer value 0
  */
 int main(void)
-#include "main.h"
 {
 	int n = 0;
 	char name[8] = "_putchar";
