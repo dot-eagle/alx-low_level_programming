582cdcbd93a05004c3c466f6c3793e3e51cc7aa4
dot-eagle <lionvic3@gmail.com>
2022-10-19 11:05:22 -0700

    8. I'm federal agent Jack Bauer, and today is the longest day of my life: Write a function that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59. Prototype: void jack_bauer(void)

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -21,7 +21,7 @@ void jack_bauer(void)
 			_putchar((b % 10) + '0');
 			_putchar(':');
 			_putchar((d / 10) + '0');
-			_putchar((d / 10) + '0');
+			_putchar((d % 10) + '0');
 			_putchar('\n');
 		}
 	}
