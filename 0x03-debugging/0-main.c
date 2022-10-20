5671c726211d65b380ddb42a9946fb4dc6b8334d
dot-eagle <lionvic3@gmail.com>
2022-10-20 02:12:53 -0700

    0. Multiple mains: create a file named 0-main.c. This file must test that the function positive_or_negative() gives the correct output when given a case of 0.

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main -test that the function positive_or_negative()
+ *
+ * Return: 0 as always
+ *
+ */
+
+int main(void)
+{
+	int r;
+
+	r = 0;
+	positive_or_negative(r);
+	return (0);
+}
