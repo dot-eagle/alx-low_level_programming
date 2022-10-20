1ddee628281f5eccd037afa4412c8e22c7b2a3dd
dot-eagle <lionvic3@gmail.com>
2022-10-20 14:03:29 -0700

    4-print_most_numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -13,9 +13,9 @@ void print_most_numbers(void)
 {
 	int x;
 
-	for (x = 0; x <= 9; x++)
+	for (x = 0; x < 9; x++)
 	{
-		if (!(x == 2 || x == 4))
+		if (x != 2 && x != 4)
 			_putchar(x);
 	}
 	_putchar('\n');
