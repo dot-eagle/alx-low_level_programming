5c3c63c17af272b77ab4905c628bd369fc3d3595
dot-eagle <lionvic3@gmail.com>
2022-10-20 08:58:19 -0700

    3. The numbers speak for themselves: Write a function that prints the numbers, from 0 to 9. Prototype: void print_numbers(void)

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_numbers -  prints the numbers, from 0 to 9
+ *
+ * Description: print out the required result
+ *
+ * Return: 0 Always 0
+ */
+
+void print_numbers(void)
+{
+	int x;
+
+	for (x = 48; x < 58; x++)
+	{
+		_putchar(x);
+	}
+
+	_putchar('\n');
+}
