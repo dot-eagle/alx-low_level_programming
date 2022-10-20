7ec00b03073d984b99df513af4128503d8d90e10
dot-eagle <lionvic3@gmail.com>
2022-10-20 11:29:10 -0700

    8. You are so much sunshine in every square inch: Write a function that prints a square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -2,28 +2,25 @@
 
 /**
  * print_square - prints a square
- * @size: argument
+ * @size: size of the square
  *
- * Description
+ * Return: return void
  *
- * Return: void
  */
 
 void print_square(int size)
 {
 	int r, t;
 
-	if (size > 0)
-	{
-		for (r = 0; r < size; r++)
-		{
-			for (t = 0; t < size; t++)
-			{
-				_putchar('\n');
-			}
-			_putchar('\n');
-		}
-	}
-	else if (size <= 0)
+	if (size <= 0)
 		_putchar('\n');
+
+	for (r = 0; r < size; r++)
+	{
+		for (t = 0; t < size; t++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
 }
