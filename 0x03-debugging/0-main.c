66c9553024171689e0e147687161dc9e59ce6411
dot-eagle <lionvic3@gmail.com>
2022-10-20 04:03:29 -0700

    2.0>978?: fix the code in 2-largest_number.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,10 +1,8 @@
 #include "main.h"
 
 /**
- * main -test that the function positive_or_negative()
- *
+ * main - tests function that prints if integer is positive or negative
  * Return: 0 as always
- *
  */
 
 int main(void)
