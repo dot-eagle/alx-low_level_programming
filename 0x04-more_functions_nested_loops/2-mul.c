1fcfd990663064e202e9b9f0affaa5d8f511c5b2
dot-eagle <lionvic3@gmail.com>
2022-10-20 08:32:44 -0700

    2. Collaboration is multiplication: Write a function that multiplies two integers. Prototype: int mul(int a, int b)

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: Always 0
+ *
+ */
+
+int mul(int a, int b)
+{
+	int results;
+
+	results = a * b;
+	return (results);
+}
