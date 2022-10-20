1d38e56ed6e033d879404fe14b5e16e1378e33c4
dot-eagle <lionvic3@gmail.com>
2022-10-20 10:02:58 -0700

    5. Numbers constitute the only universal language: Write a function that prints 10 times the numbers, from 0 to 14

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,13 +13,13 @@ void more_numbers(void)
 {
 	int x, z;
 
-	for (x = 0; x <= 10; x++)
+	for (x = 0; x < 10; x++)
 	{
 		for (z = 0; z <= 14; z++)
 		{
-			if (z >= 10)
-				_putchar('1');
-			_putchar(z % 10 + '0');
+			if (z > 9)
+				_putchar((z / 10) + '0');
+			_putchar((z % 10) + '0');
 		}
 	}
 		_putchar('\n');
