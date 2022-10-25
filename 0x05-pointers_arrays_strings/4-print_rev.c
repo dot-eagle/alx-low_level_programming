3cae950efea9984c801dd00b2244113876f55a71
dot-eagle <lionvic3@gmail.com>
2022-10-24 22:23:19 -0700

    4. I can only go one way. I've not got a reverse gear:Prototype: void print_rev(char *s)

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,13 +11,14 @@
 
 void print_rev(char *s)
 {
-	int l = 0, c;
+	int l;
 
-	while (s[c++])
-		l++;
-
-	for (c = l - 1; c >= 0; c--)
-		_putchar(s[c++]);
-
+	for (l = 0; s[l] != '\0'; l++)
+	{
+	}
+	for (l = l - 1; l >= 0; l--)
+	{
+		_putchar(s[l]);
+	}
 	_putchar('\n');
 }
