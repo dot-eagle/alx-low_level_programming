6bc7db445399bd1270e40d09febe5bfd66ce8668
dot-eagle <lionvic3@gmail.com>
2022-10-27 14:08:43 -0700

    0. strcat: Write a function that concatenates two strings; Prototype: char *_strcat(char *dest, char *src).

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -16,12 +16,13 @@ char *_strcat(char *dest, char *src)
 
 	while (*(dest + count) != '\0')
 	{
-		count++
+		count++;
 	}
 
 	while (count2 >= 0)
 	{
 		*(dest + count) = *(src + count2);
+
 		if (*(src + count2) == '\0')
 			break;
 
