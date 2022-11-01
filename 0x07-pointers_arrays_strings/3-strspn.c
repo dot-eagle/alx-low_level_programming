d64ca56b7284003115326c014de38956958bccb2
dot-eagle <lionvic3@gmail.com>
2022-10-31 20:13:46 -0700

    3-strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,22 +10,31 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int n;
+	unsigned int i, z, count, checker;
 
-	while (*s != '\0')
+	i = 0;
+	z = 0;
+	checker = 0;
+	count = 0;
+	while (s[i] != '\0')
 	{
-		while (*accept != '\0')
+		z = 0;
+		checker = 0;
+		while (accept[z] != '\0')
 		{
-			if (*s == *accept)
+			if (accept[z] == s[i])
+			{
+				count++;
+				checker = 1;
+				/* Break to early increase efficiency */
 				break;
-			n++;
-			accept++;
-
-			s++;
+			}
+			z++;
 		}
-		if (*accept == '\0')
-			break;
-		s++;
+		if (checker == 0)
+			return (count);
+		i++;
 	}
-	return (n + 1);
+	return (count);
+
 }
