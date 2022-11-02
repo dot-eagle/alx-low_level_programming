6474ca2d697e41227e8fbccdf66922be6b265ae1
dot-eagle <lionvic3@gmail.com>
2022-11-02 13:42:07 -0700

    5. Your subconscious is looking for the dreamer: Write a function that returns the natural square root of a number.

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,33 +1,33 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - returns the natural square root of a number
- * @n: natural intger number
+ * square_root - find the natural square root
+ * @i: number
+ * @j: incrementor
  *
  * Return: 0
  */
 
+int square_root(int i, int j)
+{
+	if (i * i == j)
+		return (i);
+
+	else if (i * i > j)
+		return (-1);
+
+	else
+		return (square_root(i + 1, j));
+}
+/**
+* _sqrt_recursion - returns the natural square root of a number
+* @n: natural intger number
+* Return: 0
+*/
+
 int _sqrt_recursion(int n)
 {
-	int sqrt;
+	int sqrt = 1;
 
-	if (n < 0)
-		return (-1);
-
-	else if (n <= 1 && n >= 0)
-		return (n);
-
-	if (!(n % 2))
-	{
-		if (n / 2 == 2 || n == 2)
-			return (2);
-		sqrt = _sqrt_recursion(n / 2);
-		if (n / sqrt == sqrt)
-			return (sqrt);
-		else if (sqrt > 1)
-			return (sqrt * 2);
-		return (-1);
-	}
-	else
-		return (sqrt_rec_odd(n, n - 2));
+	return (square_root(sqrt, n));
 }
