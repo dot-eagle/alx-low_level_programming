ab21c5712d8089f18b083d493cb928e64f7b7fa5
dot-eagle <lionvic3@gmail.com>
2022-11-02 13:10:20 -0700

    main_h file

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -17,6 +17,7 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
+int sqrt_rec_odd(int start, int n);
 int wildcmp(char *s1, char *s2);
 
 #endif /* _MAIN_H_ */
