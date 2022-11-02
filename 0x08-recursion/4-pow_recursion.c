387fad50e038b08c7702da1aeec2904563e22525
dot-eagle <lionvic3@gmail.com>
2022-11-02 12:04:34 -0700

    4. Once an idea has taken hold of the brain it's almost impossible to eradicate: Write a function that returns the value of x raised to the power of y

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _pow_recursion -  returns the value of x raised to the power of y
+ * @x: base number to be multiplied
+ * @y: exponential number
+ *
+ * Return: vlaue
+ */
+
+int _pow_recursion(int x, int y)
+{
+	int pow = 1;
+
+	if (y < 0)
+		return (-1);
+
+	else if (y <= 1)
+		return (1);
+
+	pow = _pow_recursion(x, --y);
+
+	return (x * pow);
+}
