4c21fed645fed0fdf407bfb878239c358c51befd
dot-eagle <lionvic3@gmail.com>
2022-11-03 01:18:19 -0700

    1010wildcmp

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * wildcmp - compares two strings and returns 1
+ * @s1: input string
+ * @s2: input string
+ *
+ * Return: 1 else 0
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	return (0);
+}
