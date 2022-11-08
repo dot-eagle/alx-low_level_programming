86c5deae5be8296351ec7373a04ee12ab08393af
dot-eagle <lionvic3@gmail.com>
2022-11-08 11:42:57 -0800

    0. Float like a butterfly, sting like a bee: Write a function that creates an array of chars, and initializes it with a specific char

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,10 +13,12 @@ char *create_array(unsigned int size, char c)
 	char *arr;
 	unsigned int x;
 
-	arr = malloc(sizeof(char) * size);
+	if (size <= 0)
+		return (0);
+	arr = malloc(size);
 
-	if (size == 0 || arr == NULL)
-		return (NULL);
+	if (arr == 0)
+		return (0);
 
 	for (x = 0, x < size; x++;)
 		arr[x] = c;
