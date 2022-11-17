19ad39f676124081cf70fb864e1d31fcbe60a1d3
dot-eagle <lionvic3@gmail.com>
2022-11-17 04:10:13 -0800

    variadic_functions_h file

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,38 @@
+#ifndef _VARIADIC_FUNCTIONS_H
+#define _VARIADIC_FUNCTIONS_H 
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stddef.h>
+#include <stdarg.h>
+
+/**
+ * struct print_ch - struct use to match operators
+ * @ch: string representing the operator
+ * @printf: function pointer to the operation to be used
+ */
+
+typedef struct print_ch
+{
+	char *ch;
+	void (*printf)(va_list);
+} p_ch;
+
+//typedef struct type variable_type;
+
+void print_numbers(const char *separator, const unsigned int n, ...);
+int sum_them_all(const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+int _putchar(char);
+void print_all(const char * const format, ...);
+
+
+void pr_int(va_list);
+void pr_char(va_list);
+void pr_float(va_list);
+void pr_str(va_list);
+
+#endif /* _VARIADIC_FUNCTIONS_H  */
