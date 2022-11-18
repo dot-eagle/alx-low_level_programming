b89acc25a67e7fa522899c5879eb6df028c05498
dot-eagle <lionvic3@gmail.com>
2022-11-18 12:58:56 -0800

    3-print_all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -64,7 +64,7 @@ void print_all(const char * const format, ...)
 	};
 
 	int b = 0, z = 0;
-	char *comma = "";
+	char *comma = " ";
 
 	va_list arguments;
 
@@ -73,6 +73,7 @@ void print_all(const char * const format, ...)
 	while (format && format[z])
 	{
 
+		b = 0;
 		while (b < 4)
 		{
 			if (identifier[b].ch == format[z])
