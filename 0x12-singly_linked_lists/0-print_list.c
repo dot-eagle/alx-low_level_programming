2622554ea2696f6e9b489fd4d100227c1cbef538
dot-eagle <lionvic3@gmail.com>
2022-11-24 03:52:05 -0800

    0-print_list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,14 +9,14 @@
 
 size_t print_list(const list_t *h)
 {
-	int h;
+	int y;
 
-	h = 0;
-	while (h != NULL)
+	y = 0;
+	while (y != NULL)
 	{
 		printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
-		h++;
+		y++;
 	}
-	return (h);
+	return (y);
 }
