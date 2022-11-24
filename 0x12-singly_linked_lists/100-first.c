1ae563b84bac8b98e78fd99624a02de10ee3f0db
dot-eagle <lionvic3@gmail.com>
2022-11-24 15:19:08 -0800

    5. The Hare and the Tortoise: Write a function that prints You're beat!

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+
+/**
+ * funhare_tortoise - prints hare and tortoise
+ *
+ * Return: void
+ */
+
+void funhare_tortoise(void)
+{
+	char *cs;
+
+	cs = "You're beat! and yet, you must allow,\nI bore my house upon my back!\n";
+
+	printf("%s", cs);
+}
