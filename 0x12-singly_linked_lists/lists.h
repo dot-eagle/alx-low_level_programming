838ebc5e3f58719c2a831064f87378604317db86
dot-eagle <lionvic3@gmail.com>
2022-11-24 14:51:18 -0800

    lists_h file

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -34,7 +34,7 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-void hare_tortoise(char)__attribute__ ((constructor));
+void hare_tortoise(char *c) __attribute__ ((constructor));
 
 
 #endif /* _LISTS_H */
