50bd62689e94f2177b59c19584df9de84af96a07
dot-eagle <lionvic3@gmail.com>
2022-11-28 08:56:36 -0800

    1. List length: Write a function that returns the number of elements in a linked listint_t list.

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * listint_len - returns the number of elements in a linked listint_t list
+ * @h:  para
+ *
+ * Returns: number of elements
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	const listint_t *dir = h;
+	size_t count = 0;
+
+	if (h == NULL)
+		return (0);
+	while (dir != NULL)
+	{
+		count++;
+		dir = dir->next;
+	}
+	return (count);
+}
