91eaa25161500200f2417214cbca504ce32288e7
dot-eagle <lionvic3@gmail.com>
2022-11-30 12:27:02 -0800

    14. Find the loop: Write a function that finds the loop in a linked list.

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,51 @@
+#include "lists.h"
+
+/**
+ * _check_and_find - Check the list and return the looping node
+ * @head: head of list to check
+ * @prev: prev node on the list
+ *
+ * Return: Node that loops, or NULL
+ */
+
+listint_t *_check_and_find(listint_t *head, listint_t *prev)
+{
+	listint_t node, *tmp;
+
+	if (head->next == NULL)
+	{
+		return (NULL);
+	}
+
+	node.next = prev;
+	/* node.addy = head; */
+	tmp = node.next;
+
+	while (tmp != NULL && tmp != head)
+	{
+		tmp = tmp->next;
+	}
+	if (tmp != NULL)
+	{
+		return (head);
+	}
+
+	return (_check_and_find(head->next, &node));
+}
+
+/**
+ * find_listint_loop - find if list has a loop
+ * @head: head of the list
+ *
+ * Return: Node that loops, or NULL
+ */
+
+listint_t *find_listint_loop(listint_t *head)
+{
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+
+	return (_check_and_find(head, NULL));
+}
