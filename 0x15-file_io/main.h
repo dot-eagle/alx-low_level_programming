706ccadb1f8e0e05b56257165cf5faec11383115
dot-eagle <lionvic3@gmail.com>
2022-12-05 20:23:35 -0800

    main_h file

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,27 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+/* headers files declarations */
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <ctype.h>
+#include <stdbool.h>
+#include <limits.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stddef.h>
+#include <stdarg.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+/* Prototype function declaration */
+
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+int _putchar(char c);
+
+#endif /* _MAIN_H */
