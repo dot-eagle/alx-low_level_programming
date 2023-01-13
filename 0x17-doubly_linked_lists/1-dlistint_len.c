edbc3dc7ae5da520a5828bc37ca96ea36c88c787
dot-eagle <lionvic3@gmail.com>
2023-01-13 09:47:39 -0800

    1. List length: Write a function that returns the number of elements in a linked dlistint_t list.

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -14,10 +14,7 @@ size_t print_dlistint(const dlistint_t *h)
 
 	while (tmpr != NULL)
 	{
-		/* Nodes_count++; */
-		/*  printf("%d\n", tmpr->n);  */
 		tmpr = tmpr->next;	/*tmpr will point to the next node*/
-
 		Nodes_count++;
 	}
 
