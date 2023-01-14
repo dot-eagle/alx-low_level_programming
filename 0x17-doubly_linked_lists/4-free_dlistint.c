9bcfa3f6a9c0c6fc92d90f007f49cd89656d8c81
dot-eagle <lionvic3@gmail.com>
2023-01-14 07:38:29 -0800

    4-free_dlistint.c gip

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,29 @@
+
+#include "lists.h"
+
+
+/**
+ * free_dlistint - frees a doubly linked list
+ * @head: pointer to ead of given list
+ *
+ * Return: nothing
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *tempr;
+
+	if (head == NULL)
+		return;
+	else
+		while (head != NULL)
+		{
+			tempr = head;
+			head = head->next;
+			head = head->prev;
+			free(tempr);
+
+		}
+	free(head);
+}
+
